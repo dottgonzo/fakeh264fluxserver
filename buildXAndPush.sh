6ab8fac71d38ff852a2dd0d34fb5efe7f8560f0f
dottgonzo <darioyzf@gmail.com>
2025-10-04 10:47:28 +0200

    ...

--- buildXAndPush.sh
+++ buildXAndPush.sh
@@ -6,8 +6,4 @@ REGISTRY_URI="dottgonzo/fakeh264fluxserver"
 
 echo building $REGISTRY_URI with version: \"master\" and \"$VERSION\"
 
-# docker buildx build --add-host gitea.harlock.network:192.168.60.170 --platform linux/amd64 -t $REGISTRY_URI:master -t $REGISTRY_URI:$VERSION --push .
-
-
-# docker buildx build --add-host gitea.harlock.network:192.168.60.170 --platform linux/amd64 -t $REGISTRY_URI:master -t $REGISTRY_URI:$VERSION --push .
 docker buildx build --platform linux/amd64,linux/arm64 -t $REGISTRY_URI:master -t $REGISTRY_URI:$VERSION --push .
